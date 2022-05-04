2f5e04a950c6a8fb3fdf7539007cbce39e8fa705
JophASL <joogasl@gmail.com>
2022-05-04 18:40:54 -0400

    Added ability to launch the bookmark inside the browser onTap of the BookmarkTile.

--- lib/helpers/bookmark_tile.dart
+++ lib/helpers/bookmark_tile.dart
@@ -1,5 +1,6 @@
 import 'package:ao3_scraper/ao3_scraper.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BookmarkTile extends StatelessWidget {
   final Work work;
@@ -10,7 +11,13 @@ class BookmarkTile extends StatelessWidget {
     return ListTile(
       title: Text("${work.title} by ${work.author}"),
       subtitle: Text(work.description),
-      onTap: () {}, // TODO: Implement onTap for ListTiles
+      onTap: () => _launchUrl(
+        Ao3Client.getURLfromWorkID(work.workID),
+      ), // TODO: Implement onTap for ListTiles
     );
+  }
+
+  void _launchUrl(String _url) async {
+    if (!await launchUrl(Uri.parse(_url))) throw 'Could not launch $_url';
   }
 }
