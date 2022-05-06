706a9650463921b545dc2270da2745fac855a85e
JophASL <joogasl@gmail.com>
2022-05-06 18:47:10 -0400

    Changed app theming a bit.

--- lib/apptheme.dart
+++ lib/apptheme.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static var light = ThemeData.light().copyWith();
+  static var light = ThemeData.light().copyWith(
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color.fromARGB(255, 153, 0, 0),
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: Color.fromARGB(255, 153, 0, 0)),
+  );
 }
